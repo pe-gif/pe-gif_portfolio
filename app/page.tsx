4fa76a5ff7f15f08efeb28246f873ce392533cc8
Peter Nguyen <153048514+pe-gif@users.noreply.github.com>
2026-02-20 17:25:49 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -57,7 +57,7 @@ const projectsData: Project[] = [
   }
 ]
 
-export default function page() {
+export default function Page() {
   return(
     <div className="min-h-screen bg-slate-100 text-slate-50 font-Helvetica selection:bg-blue-500/30">
       
