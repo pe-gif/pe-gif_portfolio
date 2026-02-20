f5bd651747676150fe9a409348e58287b1c2a9ba
Peter Nguyen <peter2002nguyen@gmail.com>
2026-02-20 13:12:39 -0500

    create site background and started navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,36 @@
-import Image from "next/image";
+import react, { useState } from 'react';
+import {Github, Linkedin, Mail, ExternalLink, Terminal, Code2, Cpu, ChevronRight } from 'lucide-react';
 
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+interface Project {
+  id: number;
+  title: string;
+  description: string;
+  tech: string[];
+  link: string;
+}
+
+const projectsData: Project[] = [
+  {
+    id: 1,
+    title: "Zero Interface Gesture AIR Tic-Tac-Toe"
+    description: "A touchless gaming experience using computer science vison, Built with python libraries, Html, Javascript, CSS to tracks finger movements via webcam to play Tic-Tac-Toe with 95% accuracy on browser."
+    tech: ["Python", "OpenCV", "Html", "CSS", "Javascript"]
+    link: "#https://github.com/pe-gif/AIR-TIC-TAC-TOE"
+  }
+]
+
+export default function App() {
+  <div className="min-h-screen bg-slate-100 text-slate-950 font-Helvetica selection:bg-blue-500/30">
+    
+    <nav className="">
+        <div className="max-w-5xl mx-auto px-6 py-4 flex justify-between items-center">
+          <span className="text-xl font-bold tracking-tighter text-white">pe-gif<span className="text-emerald-400">.dev</span></span>
+          <div className="flex gap-4">
+            <a href="#projects" className="text-sm hover:text-emerald-400 transition-colors">Projects</a>
+            <a href="#skills" className="text-sm hover:text-emerald-400 transition-colors">Skills</a>          
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+      </nav>
+  
+  </div>
 }
